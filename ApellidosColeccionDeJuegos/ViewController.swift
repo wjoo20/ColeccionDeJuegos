cc3419b7fce989dcde338de0f21893cacd83889d
Junior Osis <juniorosis20@gmail.com>
2022-10-17 12:44:39 -0700

    Initial Commit

---
+++ ApellidosColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApellidosColeccionDeJuegos
+//
+//  Created by Junior Osis on 10/17/22.
+//  Copyright © 2022 empresa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
